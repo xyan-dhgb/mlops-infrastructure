4e158a659818b9709e5323ded66c1454e747ee9e
Đinh Huỳnh Gia Bảo <baodhg@innotech-vn.com>
2026-03-08 14:51:20 +0700

    fix: add subnet_ids for second bastion host

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -36,7 +36,7 @@ module "bastion" {
   source = "../../modules/bastion-host"
 
   vpc_id           = module.vpc.vpc_id
-  subnet_id        = module.vpc.public_subnet_ids[0]
+  subnet_ids       = module.vpc.public_subnet_ids
   ssh_key_name     = var.bastion_ssh_key_name
   allowed_ssh_cidr = var.bastion_allowed_ssh_cidr
 }
