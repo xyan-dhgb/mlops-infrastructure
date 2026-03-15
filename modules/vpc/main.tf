9292e8ea6c565e5420e181677df51c16a63943fa
Đinh Huỳnh Gia Bảo <baodhg@innotech-vn.com>
2026-03-15 23:35:08 +0700

    fix(vpc): add eks cluster tags

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,7 +10,8 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 
   tags = {
-    Name = "${var.project_name}-vpc"
+    Name                                        = "${var.project_name}-vpc"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
 
