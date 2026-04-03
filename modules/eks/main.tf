0406e7f813daaccd6a5ae83685b7c34f4c839b16
Đinh Huỳnh Gia Bảo <baodhg@innotech-vn.com>
2026-04-03 22:55:43 +0700

    feat(eks): enable API_AND_CONFIG_MAP authentication mode

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -20,6 +20,11 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster_role.arn
   version  = var.cluster_version
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   vpc_config {
     subnet_ids = concat(var.public_subnet_ids, var.private_subnet_ids)
     # Attach the custom control plane security group so the cluster SG rules are applied
