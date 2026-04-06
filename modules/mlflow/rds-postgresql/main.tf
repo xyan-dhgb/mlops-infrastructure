79eedc4496687327b5fc06a138ebfdd68f2034f2
Đinh Huỳnh Gia Bảo <baodhg@innotech-vn.com>
2026-04-06 21:14:54 +0700

    fix(mlflow-rds-postgres): change the tag value for better visual

--- modules/mlflow/rds-postgresql/main.tf
+++ modules/mlflow/rds-postgresql/main.tf
@@ -3,8 +3,8 @@ resource "aws_db_subnet_group" "mlflow_db_subnet_group" {
   subnet_ids = var.subnet_ids
 
   tags = {
-    Project     = "mlops-infr"
-    Environment = "dev"
+    Project     = var.project_name
+    Environment = var.environment
   }
 }
 
