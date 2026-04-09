e9547ec7874ed09f6de5149ae43a03ba8538a369
Đinh Huỳnh Gia Bảo <baodhg@innotech-vn.com>
2026-04-09 13:02:16 +0700

    refactor: remove unused argocd, ingress-nginx and route53 modules

--- modules/argocd/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "argocd_release_name" {
-  description = "The name of the ArgoCD Helm release"
-  value       = helm_release.argocd.name
-}
-
-output "argocd_namespace" {
-  description = "The namespace where ArgoCD is installed"
-  value       = helm_release.argocd.namespace
-}
--- modules/ingress-nginx/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "namespace" {
-  description = "Kubernetes namespace where ingress-nginx is installed"
-  value       = helm_release.ingress_nginx.namespace
-}
-
-output "chart_version" {
-  description = "Deployed ingress-nginx Helm chart version"
-  value       = helm_release.ingress_nginx.version
-}
