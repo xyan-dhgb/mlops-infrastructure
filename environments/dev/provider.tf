304410ad15b36721727f49680613169d6cf155e5
Đinh Huỳnh Gia Bảo <baodhg@innotech-vn.com>
2026-06-21 19:48:45 +0700

    style(infra): auto-format hcl configuration files

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -24,6 +24,6 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
   }
 }
